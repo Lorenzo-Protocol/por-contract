ec2a53dbd22b172232b7eaa5262562536a9ca124
lorenzo <leven@lorenzo-protocol.xyz>
2024-10-16 15:47:08 +0800

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv'
 import 'hardhat-deploy'
 dotenv.config()
 
-const deployer = process.env.DEPLOY_PRIVATE_KEY || '0x' + '11'.repeat(32)
+const contract_deployer = process.env.DEPLOY_PRIVATE_KEY || '0x' + '11'.repeat(32)
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
@@ -16,27 +16,27 @@ const config: HardhatUserConfig = {
     bscTestnet: {
       chainId: 97,
       url: process.env.BSC_TESTNET_RPC_URL || '',
-      accounts: [deployer],
+      accounts: [contract_deployer],
     },
     bsc: {
       chainId: 56,
       url: process.env.BSC_MAINNET_RPC_URL || '',
-      accounts: [deployer],
+      accounts: [contract_deployer],
     },
     arbitrum_one: {
       chainId: 42161,
       url: process.env.ARB_MAINNET_RPC_URL || '',
-      accounts: [deployer],
+      accounts: [contract_deployer],
     },
     mainnet: {
       chainId: 1,
       url: process.env.ETH_MAINNET_RPC_URL || '',
-      accounts: [deployer],
+      accounts: [contract_deployer],
     },
     sepolia: {
       chainId: 11155111,
       url: process.env.SEPOLIA_RPC_URL || '',
-      accounts: [deployer],
+      accounts: [contract_deployer],
     }
   },
   namedAccounts: {
@@ -47,7 +47,8 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: {
       sepolia: process.env.ETH_MAINNET_API_KEY!,
-      bscTestnet: process.env.BSC_TESTNET_API_KEY!
+      bscTestnet: process.env.BSC_SCAN_API_KEY!,
+      bsc: process.env.BSC_SCAN_API_KEY!
     }
   }
 };
